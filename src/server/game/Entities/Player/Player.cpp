ab93c3097015ad6324b23851dbbb8a423e6bfcd9
ConanHUN <conan_513@hotmail.com>
2021-11-04 04:10:32 +0100

    Revert "Merge pull request #11 from acidmanifesto/classless-new"
    This reverts commit 23443b18bedf9b6c518d504d56ff7d0c79f5fa34, reversing
    changes made to 07af73c83be1b3e3e009a5c016c98ee082951a6a.

--- src/server/game/Entities/Player/Player.cpp
+++ src/server/game/Entities/Player/Player.cpp
@@ -12759,16 +12759,8 @@ void Player::StoreLootItem(uint8 lootSlot, Loot* loot)
 
     if (!item || item->is_looted)
     {
-		if (sConfigMgr->GetBoolDefault("AOE.LOOT.enable", true))
-        {
-            // SendEquipError(EQUIP_ERR_ALREADY_LOOTED, nullptr, nullptr); prevents error already loot from spamming
-            return;
-        }
-        else
-        {
-            SendEquipError(EQUIP_ERR_ALREADY_LOOTED, nullptr, nullptr);
-            return;
-        }
+        SendEquipError(EQUIP_ERR_ALREADY_LOOTED, nullptr, nullptr);
+        return;
     }
 
     // Xinef: exploit protection, dont allow to loot normal items if player is not master loot and not below loot threshold
--- src/server/game/Handlers/LootHandler.cpp
+++ src/server/game/Handlers/LootHandler.cpp
@@ -15,7 +15,6 @@
  * with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include "Config.h"
 #include "Corpse.h"
 #include "Creature.h"
 #include "GameObject.h"
@@ -86,62 +85,15 @@ void WorldSession::HandleAutostoreLootItemOpcode(WorldPacket& recvData)
     else
     {
         Creature* creature = GetPlayer()->GetMap()->GetCreature(lguid);
-        if (!player->GetGroup() && creature && sConfigMgr->GetBoolDefault("AOE.LOOT.enable", true))
+
+        bool lootAllowed = creature && creature->IsAlive() == (player->getClass() == CLASS_ROGUE && creature->loot.loot_type == LOOT_PICKPOCKETING);
+        if (!lootAllowed || !creature->IsWithinDistInMap(_player, INTERACTION_DISTANCE))
         {
-            int                    i     = 0;
-            float                  range = 30.0f;
-            Creature*              c     = nullptr;
-            std::list<Creature*> creaturedie;
-            player->GetDeadCreatureListInGrid(creaturedie, range);
-            for (std::list<Creature*>::iterator itr = creaturedie.begin(); itr != creaturedie.end(); ++itr)
-            {
-                c    = *itr;
-                loot = &c->loot;
-
-                uint8 maxSlot = loot->GetMaxSlotInLootFor(player);
-                for (i = 0; i < maxSlot; ++i)
-                {
-                    if (LootItem* item = loot->LootItemInSlot(i, player))
-                    {
-                        if (player->AddItem(item->itemid, item->count))
-                        {
-                            player->SendNotifyLootItemRemoved(lootSlot);
-                            player->SendLootRelease(player->GetLootGUID());
-                        }
-                        else
-                        {
-                            player->SendItemRetrievalMail(item->itemid, item->count);
-                            player->GetSession()->SendAreaTriggerMessage("Your items has been mailed to you.");
-                        }
-                    }
-                }
-
-                // This if covers a issue with skinning being infinite by Aokromes
-                if (!creature->IsAlive())
-                {
-                    creature->AllLootRemovedFromCorpse();
-                }
-
-                loot->clear();
-
-                if (loot->isLooted() && loot->empty())
-                {
-                    c->RemoveFlag(UNIT_DYNAMIC_FLAGS, UNIT_DYNFLAG_LOOTABLE);
-                    c->AllLootRemovedFromCorpse();
-                }
-            }
+            player->SendLootError(lguid, lootAllowed ? LOOT_ERROR_TOO_FAR : LOOT_ERROR_DIDNT_KILL);
+            return;
         }
-        else
-        {
-            bool lootAllowed = creature && creature->IsAlive() == (player->getClass() == CLASS_ROGUE && creature->loot.loot_type == LOOT_PICKPOCKETING);
-            if (!lootAllowed || !creature->IsWithinDistInMap(_player, INTERACTION_DISTANCE))
-            {
-                player->SendLootError(lguid, lootAllowed ? LOOT_ERROR_TOO_FAR : LOOT_ERROR_DIDNT_KILL);
-                return;
-            }
 
-            loot = &creature->loot;
-        }
+        loot = &creature->loot;
     }
 
     player->StoreLootItem(lootSlot, loot);
@@ -250,27 +202,6 @@ void WorldSession::HandleLootMoneyOpcode(WorldPacket& /*recvData*/)
         }
         else
         {
-            ObjectGuid lguid    = player->GetLootGUID();
-            Creature*  creature = GetPlayer()->GetMap()->GetCreature(lguid);
-            if (creature && sConfigMgr->GetBoolDefault("AOE.LOOT.enable", true))
-            {
-                if (!player->GetGroup())
-                {
-                    float                  range = 30.0f;
-                    uint32                 gold  = 0;
-                    Creature*              c     = nullptr;
-                    std::list<Creature*>   creaturedie;
-                    player->GetDeadCreatureListInGrid(creaturedie, range);
-                    for (std::list<Creature*>::iterator itr = creaturedie.begin(); itr != creaturedie.end(); ++itr)
-                    {
-                        c    = *itr;
-                        loot = &c->loot;
-                        gold += loot->gold;
-                        loot->gold = 0;
-                    }
-                    loot->gold = gold;
-                }
-            }
             player->ModifyMoney(loot->gold);
             player->UpdateAchievementCriteria(ACHIEVEMENT_CRITERIA_TYPE_LOOT_MONEY, loot->gold);
 
