2273fafe8659de5df3cbd85d57efa905f3cc9acb
小女孩 <xuehyc@163.com>
2023-04-21 20:52:23 +0800

    添加所需目录

--- dev/配置文件/分发配置文件(由此处到bin目录).bat
+++ dev/配置文件/分发配置文件(由此处到bin目录).bat
@@ -2,10 +2,11 @@ rem 注意此批处理,必须设置文本格式为ANSI,不然乱码,无法正常执行
 
 mkdir "D:\Program Files (x86)\WOWDIY\ACore\out\build\x64-Debug\bin\configs\"
 mkdir "D:\Program Files (x86)\WOWDIY\ACore\out\build\x64-Release\bin\configs\"
+mkdir "D:\Program Files (x86)\WOWDIY\ACore\out\build\x64-Debug\bin\configs\modules\"
+mkdir "D:\Program Files (x86)\WOWDIY\ACore\out\build\x64-Release\bin\configs\modules\"
 
-
-copy /Y "D:\Program Files (x86)\WOWDIY\ACore\dev\配置文件\playerbots.conf.dist" "D:\Program Files (x86)\WOWDIY\ACore\out\build\x64-Release\bin\configs\"
-copy /Y "D:\Program Files (x86)\WOWDIY\ACore\dev\配置文件\playerbots.conf.dist" "D:\Program Files (x86)\WOWDIY\ACore\out\build\x64-Debug\bin\configs\"
+copy /Y "D:\Program Files (x86)\WOWDIY\ACore\dev\配置文件\playerbots.conf.dist" "D:\Program Files (x86)\WOWDIY\ACore\out\build\x64-Release\bin\configs\modules\"
+copy /Y "D:\Program Files (x86)\WOWDIY\ACore\dev\配置文件\playerbots.conf.dist" "D:\Program Files (x86)\WOWDIY\ACore\out\build\x64-Debug\bin\configs\modules\"
 
 copy /Y "D:\Program Files (x86)\WOWDIY\ACore\dev\配置文件\authserver.conf.dist" "D:\Program Files (x86)\WOWDIY\ACore\out\build\x64-Debug\bin\configs\authserver.conf"
 copy /Y "D:\Program Files (x86)\WOWDIY\ACore\dev\配置文件\worldserver.conf.dist" "D:\Program Files (x86)\WOWDIY\ACore\out\build\x64-Debug\bin\configs\worldserver.conf"
