1fe4b727858ae7a29578fb8f564d58347e0c312f
小女孩 <xuehyc@163.com>
2023-04-22 07:46:30 +0800

    修正分发额外的DLL批处理

--- dev/额外的dll/分发额外的DLL(由此处到bin目录).bat
+++ dev/额外的dll/分发额外的DLL(由此处到bin目录).bat
@@ -1,6 +1,6 @@
 rem 注意此批处理,必须设置文本格式为ANSI,不然乱码,无法正常执行
 
 
-copy "D:\Program Files (x86)\WOWDIY\ACore\dev\额外的dll\*.dll" "D:\Program Files (x86)\WOWDIY\ACore\out\build\x64-Release\bin\"
+copy "D:\Program Files (x86)\WOWDIY\ACore\dev\额外的dll\*.dll" "D:\Program Files (x86)\WOWDIY\ACore\out\build\x64-Debug\bin\"
 
-copy "D:\Program Files (x86)\WOWDIY\ACore\dev\配置文件\worldserver.conf.dist" "D:\Program Files (x86)\WOWDIY\ACore\out\build\x64-Release\bin\"
+copy "D:\Program Files (x86)\WOWDIY\ACore\dev\额外的dll\*.dll" "D:\Program Files (x86)\WOWDIY\ACore\out\build\x64-Release\bin\"
