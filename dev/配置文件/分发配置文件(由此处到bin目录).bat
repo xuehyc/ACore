8207b62767f772840d601f1bae12e7a915c4763a
小女孩 <xuehyc@163.com>
2023-04-23 13:59:54 +0800

    添加eluna脚本(https://github.com/ElunaLuaEngine/Scripts)

--- dev/配置文件/分发配置文件(由此处到bin目录).bat
+++ dev/配置文件/分发配置文件(由此处到bin目录).bat
@@ -8,6 +8,10 @@ mkdir "D:\Program Files (x86)\WOWDIY\ACore\out\build\x64-Release\bin\configs\mod
 copy /Y "D:\Program Files (x86)\WOWDIY\ACore\dev\配置文件\playerbots.conf.dist" "D:\Program Files (x86)\WOWDIY\ACore\out\build\x64-Release\bin\configs\modules\"
 copy /Y "D:\Program Files (x86)\WOWDIY\ACore\dev\配置文件\playerbots.conf.dist" "D:\Program Files (x86)\WOWDIY\ACore\out\build\x64-Debug\bin\configs\modules\"
 
+copy /Y "D:\Program Files (x86)\WOWDIY\ACore\dev\配置文件\mod_LuaEngine.conf.dist" "D:\Program Files (x86)\WOWDIY\ACore\out\build\x64-Release\bin\configs\modules\"
+copy /Y "D:\Program Files (x86)\WOWDIY\ACore\dev\配置文件\mod_LuaEngine.conf.dist" "D:\Program Files (x86)\WOWDIY\ACore\out\build\x64-Debug\bin\configs\modules\"
+
+
 copy /Y "D:\Program Files (x86)\WOWDIY\ACore\dev\配置文件\authserver.conf.dist" "D:\Program Files (x86)\WOWDIY\ACore\out\build\x64-Debug\bin\configs\authserver.conf"
 copy /Y "D:\Program Files (x86)\WOWDIY\ACore\dev\配置文件\worldserver.conf.dist" "D:\Program Files (x86)\WOWDIY\ACore\out\build\x64-Debug\bin\configs\worldserver.conf"
 
