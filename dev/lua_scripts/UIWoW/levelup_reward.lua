19203c43b345e9e2b1098795e4b5bb13d0339764
小女孩 <xuehyc@163.com>
2023-04-23 17:41:19 +0800

    去除中文,以防英文客户端乱码

--- dev/lua_scripts/UIWoW/levelup_reward.lua
+++ dev/lua_scripts/UIWoW/levelup_reward.lua
@@ -1,6 +1,6 @@
 ﻿function levelup_reward(event, player, oldLevel)    --升级奖励1金
 player:ModifyMoney(10000)
 player:SendBroadcastMessage("You owned 1 Gold for your levelup.")
-player:SendBroadcastMessage("升级奖励1金")
+--player:SendBroadcastMessage("升级奖励1金")
 end  
 RegisterPlayerEvent(13, levelup_reward)
