d970d3b2a48de75bf1e8c3c3150d53ed2ba259a8
小女孩 <xuehyc@163.com>
2023-04-24 10:36:14 +0800

    尝试修复字符串转数字错误

--- dev/lua_scripts/UIWoW/levelup_reward.lua
+++ dev/lua_scripts/UIWoW/levelup_reward.lua
@@ -1,6 +1,12 @@
 ﻿function levelup_reward(event, player, oldLevel)    --升级奖励金币
 --player:ModifyMoney(10000) --奖励1金
+--oldLevel=getuint32(oldLevel)
+
+--尝试更换数据类型,之前会报字符串转数值错误(错误如下:)
+--lua_scripts/UIWoW/levelup_reward.lua:14(行号): attempt to perform arithmetic on a string value
+
 player:ModifyMoney(oldLevel*1000)
+
 --奖励上一级*1000银币,如1级升到2级,则奖励1*1000银币,即0.1金.
 --2级升3级,则奖励2*1000银币,即0.2金.
 --不奖励固定值的原因是,前期给1金是很庞大的数值,虽然对玩家来说是便利了些,但也丧失了积攒金币的乐趣.
