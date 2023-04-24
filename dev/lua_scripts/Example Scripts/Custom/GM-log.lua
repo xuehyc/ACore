97d44a02d94a86a897dbcac15f5fd5e13308848e
小女孩 <xuehyc@163.com>
2023-04-24 10:14:10 +0800

    更新提示,使之更加匹配

--- dev/lua_scripts/Example Scripts/Custom/GM-log.lua
+++ dev/lua_scripts/Example Scripts/Custom/GM-log.lua
@@ -1,14 +1,14 @@
-﻿print "---------- GM Login/Logout Module ---"
+﻿print "---------- GM Login/Logout Module Loading---"
 
 local function GMLogin (event, player)
-    print "GM log in module loading..."
+    print "GM log in..."
     if player:GetGMRank() > 1 then
         SendWorldMessage("Lord |CFFFF0303"..player:GetName().."|r is among us.")
 	end
 end
 
 local function GMLogout (event, player)
-    print "GM log out module loading..."
+    print "GM log out..."
     if player:GetGMRank() > 1 then
         SendWorldMessage("Lord |CFFFF0303"..player:GetName().."|r gone.")
     end
