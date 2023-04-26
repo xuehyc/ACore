e46df575c26fdb62f56c4638ad6ee61f22de8434
小女孩 <xuehyc@163.com>
2023-04-26 18:22:34 +0800

    修正语法错误

--- dev/lua_scripts/UIWoW/levelup_reward.lua
+++ dev/lua_scripts/UIWoW/levelup_reward.lua
@@ -11,7 +11,7 @@ player:ModifyMoney(oldLevel*1000)
 --2级升3级,则奖励2*1000银币,即0.2金.
 --不奖励固定值的原因是,前期给1金是很庞大的数值,虽然对玩家来说是便利了些,但也丧失了积攒金币的乐趣.
 --后期给1金币,又太少了,所以给了个变动值.
-player:SendBroadcastMessage("You owned "..(oldLevel*1000)/10000.."Gold for your levelup.")--好像没起作用,试试下句
+player:SendBroadcastMessage("You owned "..((oldLevel*1000)/10000).."Gold for your levelup.")--好像没起作用,试试下句
 player:SendBroadcastMessage("You owned some Gold for your levelup.")
 --player:SendBroadcastMessage("升级奖励1金")
 end  
