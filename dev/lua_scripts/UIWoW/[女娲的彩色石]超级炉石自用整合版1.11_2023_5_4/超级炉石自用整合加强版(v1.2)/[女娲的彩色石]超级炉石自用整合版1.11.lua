69e93d64e7b0c102f86edf695b231416f813fbbd
小女孩 <xuehyc@163.com>
2023-06-20 10:18:15 +0800

    格式化

--- dev/lua_scripts/UIWoW/[女娲的彩色石]超级炉石自用整合版1.11_2023_5_4/超级炉石自用整合加强版(v1.2)/[女娲的彩色石]超级炉石自用整合版1.11.lua
+++ dev/lua_scripts/UIWoW/[女娲的彩色石]超级炉石自用整合版1.11_2023_5_4/超级炉石自用整合加强版(v1.2)/[女娲的彩色石]超级炉石自用整合版1.11.lua
@@ -2241,17 +2241,17 @@ local Menu={--菜单页面
 	},
 
     [MMENU+0x60]={--原神_变身_芭芭拉的衣橱    此角色应为芭芭拉_双马尾
-        {FUNC, "|TInterface/ICONS/ys_app:32:32|t白衣短筒蓝袜",            ST.Morph_45600,	GOSSIP_ICON_TRAINER},--成功
+        {FUNC, "|TInterface/ICONS/ys_app:32:32|t白衣短筒蓝袜",          ST.Morph_45600,	GOSSIP_ICON_TRAINER},--成功
         {FUNC, "|TInterface/ICONS/ys_app:32:32|t白衣长筒红袜", 	        ST.Morph_45601,	GOSSIP_ICON_TRAINER},--成功
         {FUNC, "|TInterface/ICONS/ys_app:32:32|t白衣长筒红袜", 	        ST.Morph_45602,	GOSSIP_ICON_TRAINER},--成功
         {FUNC, "|TInterface/ICONS/ys_app:32:32|t白衣长筒白袜", 	        ST.Morph_45603,	GOSSIP_ICON_TRAINER},--成功
         {FUNC, "|TInterface/ICONS/ys_app:32:32|t黑衣无袜", 	            ST.Morph_45604,	GOSSIP_ICON_TRAINER},--成功
         {FUNC, "|TInterface/ICONS/ys_app:32:32|t黑衣蒙眼黑色毛袜", 	    ST.Morph_45605,	GOSSIP_ICON_TRAINER},--成功
         {FUNC, "|TInterface/ICONS/ys_app:32:32|t黑衣长筒毛袜", 	        ST.Morph_45606,	GOSSIP_ICON_TRAINER},--成功
-        {FUNC, "|TInterface/ICONS/ys_app:32:32|t白衣红裙摆长筒白袜", 	    ST.Morph_45607,	GOSSIP_ICON_TRAINER},--成功
+        {FUNC, "|TInterface/ICONS/ys_app:32:32|t白衣红裙摆长筒白袜", 	ST.Morph_45607,	GOSSIP_ICON_TRAINER},--成功
         {FUNC, "|TInterface/ICONS/ys_app:32:32|t白衣黑裙摆长筒彩虹袜", 	ST.Morph_45608,	GOSSIP_ICON_TRAINER},--成功
-        {FUNC, "|TInterface/ICONS/ys_app:32:32|t彩虹上衣黑裙摆长筒黑袜",  ST.Morph_45609,	GOSSIP_ICON_TRAINER},--成功
-        {FUNC, "|TInterface/ICONS/ys_app:32:32|t彩虹上衣黑裙摆无袜", 	    ST.Morph_45610,	GOSSIP_ICON_TRAINER},--成功
+        {FUNC, "|TInterface/ICONS/ys_app:32:32|t彩虹上衣黑裙摆长筒黑袜",ST.Morph_45609,	GOSSIP_ICON_TRAINER},--成功
+        {FUNC, "|TInterface/ICONS/ys_app:32:32|t彩虹上衣黑裙摆无袜", 	ST.Morph_45610,	GOSSIP_ICON_TRAINER},--成功
         {FUNC, "|TInterface/ICONS/ys_app:32:32|t彩虹上衣黑裙摆彩虹袜", 	ST.Morph_45611,	GOSSIP_ICON_TRAINER},--成功
 	},
 
