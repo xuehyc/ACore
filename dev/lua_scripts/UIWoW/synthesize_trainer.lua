b9a6b8ebf6e7ff435fc06f5bb435aa64a08ee301
小女孩 <xuehyc@163.com>
2023-07-07 16:20:20 +0800

    添加所需SQL

--- dev/lua_scripts/UIWoW/synthesize_trainer.lua
+++ dev/lua_scripts/UIWoW/synthesize_trainer.lua
@@ -1,4 +1,15 @@
+﻿--[[
+之前用源码的方式编译综合训练师，遇到了各种问题，
+也不太会改，就自己写了个lua版本的。在az端里面测试过了没有问题。
+默认的npc是190016，可以自行修改。
 
+SQL如下
+1.	INSERT INTO `acore_world`.`creature_template`(`entry`, `difficulty_entry_1`, `difficulty_entry_2`, `difficulty_entry_3`, `KillCredit1`, `KillCredit2`, `modelid1`, `modelid2`, `modelid3`, `modelid4`, `name`, `subname`, `IconName`, `gossip_menu_id`, `minlevel`, `maxlevel`, `exp`, `faction`, `npcflag`, `speed_walk`, `speed_run`, `speed_swim`, `speed_flight`, `detection_range`, `scale`, `rank`, `dmgschool`, `DamageModifier`, `BaseAttackTime`, `RangeAttackTime`, `BaseVariance`, `RangeVariance`, `unit_class`, `unit_flags`, `unit_flags2`, `dynamicflags`, `family`, `trainer_type`, `trainer_spell`, `trainer_class`, `trainer_race`, `type`, `type_flags`, `lootid`, `pickpocketloot`, `skinloot`, `PetSpellDataId`, `VehicleId`, `mingold`, `maxgold`, `AIName`, `MovementType`, `HoverHeight`, `HealthModifier`, `ManaModifier`, `ArmorModifier`, `ExperienceModifier`, `RacialLeader`, `movementId`, `RegenHealth`, `mechanic_immune_mask`, `spell_school_immune_mask`, `flags_extra`, `ScriptName`, `VerifiedBuild`) VALUES (190016, 0, 0, 0, 0, 0, 31833, 0, 0, 0, '星邃', '综合训练师', 'Speak', 61032, 80, 80, 0, 35, 51, 1, 1.14286, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 2, 0, 1, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, '', 0, 1, 1, 1, 1, 0, 1, 0, 1, 0, 0, 2, '', 0);
+2.	
+
+
+
+]]
 print(">>Script: synthesize trainer  loading...OK")
 --local npcid = 24411
 --触发物品ID--
