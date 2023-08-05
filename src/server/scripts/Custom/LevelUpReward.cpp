6cbbe6d322b4210477f11f7d533898f64c909255
小女孩 <xuehyc@163.com>
2023-08-05 09:04:57 +0800

    升级奖励宠物天赋

--- src/server/scripts/Custom/LevelUpReward.cpp
+++ src/server/scripts/Custom/LevelUpReward.cpp
@@ -1,4 +1,5 @@
 #include "Config.h"
+#include "Pet.h"
 
 class LevelUpReward : public PlayerScript
 {
@@ -6,13 +7,15 @@ public:
 	LevelUpReward() : PlayerScript("LevelUpReward") { }
 	void OnLevelChanged(Player * player, uint8 oldLevel)
 	{
+        uint8 levelupReward_petFreeTalentPoints = 2;    //升级奖励的宠物天赋数量
 		if (sConfigMgr->GetBoolDefault("LevelUpReward", true))  //默认开启
 		{
             if (oldLevel == 80)
                 ;
-                
-                
-                
+            Pet* pet=player->GetPet();
+           
+            uint8 pet_freeTalentPoints =pet->GetFreeTalentPoints();
+            pet->SetFreeTalentPoints(pet_freeTalentPoints+ levelupReward_petFreeTalentPoints);//奖励宠物天赋
 		}
 	}
 };
