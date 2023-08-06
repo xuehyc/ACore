2724665aa1f359f92de7a1b57aea4e4d07742556
小女孩 <xuehyc@163.com>
2023-08-06 15:28:56 +0800

    添加挑战模式配置及批处理

--- dev/配置文件/分发配置文件(由源码到bin目录).bat
+++ dev/配置文件/分发配置文件(由源码到bin目录).bat
@@ -5,6 +5,10 @@ mkdir "D:\Program Files (x86)\WOWDIY\ACore\out\build\x64-Release\bin\configs\"
 mkdir "D:\Program Files (x86)\WOWDIY\ACore\out\build\x64-Debug\bin\configs\modules\"
 mkdir "D:\Program Files (x86)\WOWDIY\ACore\out\build\x64-Release\bin\configs\modules\"
 
+copy /Y "D:\Program Files (x86)\WOWDIY\ACore\modules\mod-challenge-modes\conf\challenge_modes.conf.dist" "D:\Program Files (x86)\WOWDIY\ACore\out\build\x64-Release\bin\configs\modules\"
+
+copy /Y "D:\Program Files (x86)\WOWDIY\ACore\modules\mod-challenge-modes\conf\challenge_modes.conf.dist" "D:\Program Files (x86)\WOWDIY\ACore\out\build\x64-Debug\bin\configs\modules\"
+
 copy /Y "D:\Program Files (x86)\WOWDIY\ACore\dev\配置文件\playerbots.conf.dist" "D:\Program Files (x86)\WOWDIY\ACore\out\build\x64-Release\bin\configs\modules\"
 copy /Y "D:\Program Files (x86)\WOWDIY\ACore\dev\配置文件\playerbots.conf.dist" "D:\Program Files (x86)\WOWDIY\ACore\out\build\x64-Debug\bin\configs\modules\"
 
